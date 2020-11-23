6ac916ac1d9e5dc320bdc716ed82642c80556f96
Jason-We <1154844946@qq.com>
2020-11-23 22:24:00 +0800

    add vue-print-nb

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import './plugins/element.js'
 import JsonExcel from 'vue-json-excel'
 import excel_export from './utils/excel_export'
+import Print from 'vue-print-nb'
+Vue.use(Print);
 
 Vue.config.productionTip = false
 Vue.component('downloadExcel', JsonExcel)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="doPrint">执行打印</el-button>
+    <div ref="print" id="printMe">
+      <p>打印内容</p>
+      <p class="no-print">非打印内容 方法1：添加no-print样式类</p>
+      <p class="className">非打印内容 方法2：自定义类名</p>
+    </div>
+    <el-button v-print="printObj">打印</el-button>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        printObj: {
+          id: "printMe",
+          popTitle: 'good print',
+        }
+      };
+    },
+    methods: {
+      print() {
+        this.$print(this.$refs.print, { 'no-print': '.className' });
+      },
+      doPrint() {
+        console.log('获取后台数据，生成打印格式');
+
+      }
+    },
+  }
+</script>
